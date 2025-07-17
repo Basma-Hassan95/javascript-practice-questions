e3b07772648322ef8113230f772a9118072bbca5
Basma-Hassan95 <basmakhan220@gmail.com>
2025-07-18 01:26:48 +0500

    first commit

---
+++ Arrays with Loops/app.js
@@ -0,0 +1,72 @@
+// Arrays With Loops
+
+//Create an array fruits = ["apple", "banana", "mango"] and print each fruit using a for loop.
+
+// let fruits = ["apple", "banana", "mango"]
+// for(let i = 0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+    
+// }
+
+
+// Use for...of loop to print all elements in colors = ["red", "green", "blue"].
+
+// let colors = ["red","green","blue"]
+
+// for(let i = 0;i<colors.length;i++){
+//     console.log(colors[i]);
+    
+// }
+
+// Print each character of the string "JavaScript" using a for loop.
+
+// let str = "Javascript";
+
+// for(let i = 0;i<str.length;i++){
+//     console.log(str[i]);
+    
+// }
+
+// Create an array numbers = [1, 2, 3, 4, 5] and print only even numbers using a for loop.
+
+// let numbers = [1,2,3,4,5]
+
+// for(let i = 0;i<numbers.length;i++){
+
+//     if(numbers[i] % 2 === 0){
+//         console.log(numbers[i]);
+        
+//     }
+// }
+
+// Print the square of each number in [2, 4, 6, 8] using a loop.
+
+// let numbers = [2, 4, 6, 8]
+
+
+
+// for(let i = 0;i<numbers.length;i++){
+
+//     numbers[i] *= numbers[i]
+
+    
+    
+// }
+// console.log(numbers);
+
+// Create an array of 5 numbers, calculate their sum using a loop.
+
+// let arr = [1,2,3,4,5]
+
+// let sum = 0
+
+// for(let i = 0;i<arr.length;i++){
+
+//     sum += arr[i]  // sum = sum + arr[i]
+//                     // sum = 0 + 1 = 1
+//                     // sum = 1 + 2 = 3
+//                     // sum = 3 + 3 = 6
+//                     // sum = 6 + 4 = 10
+//                     // sum = 10 + 5 = 15
+// }
+// console.log(sum);
